1e853c8678426204a1ead6a04f7883315f1f7de4
kush103 <133690205+kush103@users.noreply.github.com>
2023-05-15 22:35:42 +0530

    Add files via upload

---
+++ frontend/src/components/About.jsx
@@ -0,0 +1,9 @@
+const About = () => {
+    return (
+        <>
+            <h1 style={{ color: "orangedRed", fontSize: "3em", fontFamily: "cursive" }}>Kush Patel</h1>
+        </>
+    )
+}
+
+export default About;
